157bb8498c6f4eb7d1b7ad4ae5e2e0c39da7b999
srivatsan <vatsan_k@hotmail.com>
2024-12-03 17:38:28 +0530

    Day2 End of Day commit

---
+++ scripts/quickstart.js
@@ -0,0 +1,48 @@
+var obj = {};
+
+obj.addHeading = function (headingText) {
+  var heading = document.getElementById("maj-heading");
+
+  heading.innerHTML = headingText;
+
+  heading.style.color = "white";
+
+  heading.className = "centerText";
+};
+
+obj.hightLight = function () {
+  var links = document.querySelectorAll("a");
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseover", function () {
+      this.style.borderBottom = "3px solid white";
+    });
+  }
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("mouseout", function () {
+      this.style.borderBottom = "";
+    });
+  }
+};
+
+obj.underStandJS = function () {
+  var header = document.querySelector("h3");
+
+  //   header.addEventListener("click", function () {
+  //     alert("hi");
+  //   });
+
+  // should not write obj.callBack() - in that case function will invoked
+  // even before the event is fired
+  header.addEventListener("click", obj.callBack("hello"));
+};
+
+obj.callBack = function (text) {
+  return function () {
+    alert(text);
+  };
+};
+
+obj.addHeading("Java Script Ninja");
+obj.hightLight();
+obj.underStandJS();
