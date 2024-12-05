9524a92df7a97e6a1a41ed45044dca33d42dfbda
srivatsan <vatsan_k@hotmail.com>
2024-12-05 14:17:12 +0530

    Comments Added

--- scripts/filter_table.js
+++ scripts/filter_table.js
@@ -5,17 +5,22 @@ function Student(rollNumber, studentname, mark, branch) {
   this.branch = branch;
 }
 
+// ================== Filter Example Starts Here =================
+
 var obj = {}; // using object literal syntax a blank object is created
+
+// Attach Event Method takes table Id and Search Field Id and Column Selctor
 
 obj.attachEvent = function (tableId, elementId, selector) {
   let srchFld = document.querySelector(elementId);
 
   srchFld.addEventListener("keyup", (event) => {
     let valToSrch = event.target.value;
-    console.log(valToSrch);
     obj.filterTableData(tableId, valToSrch, selector);
   });
 };
+
+// Filtering the Table Data - Takes Table id value to search and the Column Selector
 
 obj.filterTableData = function (tableId, valToSrch, selector) {
   let srch = "#" + tableId;
@@ -36,7 +41,8 @@ obj.filterTableData = function (tableId, valToSrch, selector) {
     }
   }
 };
-obj.attachEvent("first", "#srchByName", "td:nth-child(2)");
+
+// ========= Render Table   =================
 
 obj.renderTable = function (parentId, tableId, className) {
   let customTable = document.createElement("table");
@@ -47,6 +53,7 @@ obj.renderTable = function (parentId, tableId, className) {
   document.getElementById(parentId).appendChild(customTable);
 };
 
+// == Render Tablde Heading =============
 obj.renderHeading = function (tableId, ...colNames) {
   let customTable = document.getElementById(tableId);
 
@@ -63,6 +70,8 @@ obj.renderHeading = function (tableId, ...colNames) {
 
   document.getElementById(tableId).querySelector("thead").appendChild(row);
 };
+
+// ===== Render Body Conent ===============
 
 obj.renderContent = function (tableId, studentList) {
   var row = null;
@@ -85,6 +94,7 @@ obj.renderContent = function (tableId, studentList) {
     document.getElementById(tableId).querySelector("tbody").appendChild(row);
   });
 };
+
 obj.renderTable("custom-table", "cust-table", "table-style");
 
 obj.renderHeading("cust-table", "Roll Number", "studentname", "mark", "Branch");
@@ -97,10 +107,10 @@ let studentList = [ram, shyam, kumar];
 
 obj.renderContent("cust-table", studentList);
 
+//==== Calling the Methods
+
+// Attach Event called with table id , search field Id and table data column
+
+obj.attachEvent("first", "#srchByName", "td:nth-child(2)");
+
 obj.attachEvent("cust-table", "#srchByBranch", "td:nth-child(4)");
-
-// obj.renderContent(ram,shyam)  we can make it work with rest operator
-
-// tableBody.querySelectorAll("tr").forEach((eachRow)=>{
-
-// })
